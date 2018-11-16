64176a682debf108113885d7e9d3493e60f37717
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2018-11-16 15:29:18 +0700

    add main application and UImanager

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+// 2018 Prog Meth Project Create By Tong & Pinn
+
+/*
+ *  main application for launching game
+ * 
+ */
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+		
+		UIManager ui = new UIManager();
+		
+		primaryStage = ui.getMainStage();
+		
+		primaryStage.setTitle("Animal Rescue");				// game name 
+		primaryStage.setFullScreen(true);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/UIManager.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
+public class UIManager {
+	
+	private Stage mainStage;
+	private Scene mainScene;
+	private AnchorPane uiRoot;
+	
+	// constructor
+	public UIManager() {
+		uiRoot = new AnchorPane();
+		mainScene = new Scene(uiRoot);
+		mainStage = new Stage();
+		mainStage.setScene(mainScene);
+	}
+	
+	// get main stage
+	public Stage getMainStage() {
+		return mainStage;
+	}
+	
+
+}
