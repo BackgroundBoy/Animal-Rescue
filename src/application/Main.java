8e791bffc077774f7512f1e3b710cdacbddc956f
Pollawat Hongwimol <6030400021@student.chula.ac.th>
2018-11-22 15:09:35 +0700

    Add font loader

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 // 2018 Prog Meth Project Create By Tong & Pinn
@@ -14,7 +15,14 @@ public class Main extends Application {
 			
 	@Override
 	public void start(Stage primaryStage) {
-	
+
+		// Font Loader for import all fonts to the game
+		Font.loadFont(ClassLoader.getSystemResourceAsStream("fonts/BACKTO1982.TTF"), 100);
+		Font.loadFont(ClassLoader.getSystemResourceAsStream("fonts/Blox2.ttf"), 100);
+		Font.loadFont(ClassLoader.getSystemResourceAsStream("fonts/joystix_monospace.ttf"), 100);
+		Font.loadFont(ClassLoader.getSystemResourceAsStream("fonts/operational amplifier.ttf"), 100);
+		Font.loadFont(ClassLoader.getSystemResourceAsStream("fonts/pixFont.TTF"), 100);
+		
 		UIManager ui = new UIManager();
 		
 		primaryStage = ui.getMainStage();
